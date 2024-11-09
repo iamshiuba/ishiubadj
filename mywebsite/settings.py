23bc7b46df2e7c24449d20ec6244244d7a338d28
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-11-09 00:07:13 -0300

    bruh

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'ishiubadj.onrender.com',
+    '127.0.0.1'
 ]
 
 
@@ -130,6 +131,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
