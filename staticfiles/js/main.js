8791bcbae0e4729fa62853c8ef6b74ba69e3fbf3
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-11-19 17:09:58 -0300

    Update staticfiles

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,7 +1,3 @@
-/**
- * The `initializeLanguage` function sets the selected language based on the value stored in
- * localStorage and checks the corresponding radio button.
- */
 function initializeLanguage() {
   const selectedLanguage = localStorage.getItem("selectedLanguage") || "en";
   setLanguage(selectedLanguage);
@@ -13,13 +9,6 @@ function initializeLanguage() {
     radioButton.checked = true;
   }
 }
-
-/**
- * The function `setLanguage` allows for dynamic translation of elements on a webpage based on the
- * selected language and stores the selected language in local storage.
- * @param language - The `language` parameter is a string that represents the selected language for
- * translation.
- */
 function setLanguage(language) {
   const elementsToTranslate = document.querySelectorAll("[data-translate]");
   elementsToTranslate.forEach((element) => {
@@ -34,22 +23,6 @@ function setLanguage(language) {
   document.documentElement.setAttribute("lang", language);
 }
 
-/* The code `document.querySelectorAll('input[name="btnradio"]').forEach((radio) => {
-    radio.addEventListener("change", function () {
-      setLanguage(this.value);
-    });
-  });` is selecting all input elements with the attribute `name="btnradio"` on the webpage. It then
-  iterates over each of these input elements using the `forEach` method. */
-document.querySelectorAll('input[name="btnradio"]').forEach((radio) => {
-  radio.addEventListener("change", function () {
-    setLanguage(this.value);
-  });
-});
-
-/**
- * The above JavaScript functions initialize and toggle a dark theme for a webpage based on user
- * preference and store the theme choice in local storage.
- */
 function initializeTheme() {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
@@ -60,11 +33,6 @@ function initializeTheme() {
     document.getElementById("theme-switcher").checked = false; // Set checkbox to reflect the theme
   }
 }
-
-/**
- * The function `toggleTheme` toggles between dark and light themes on a webpage based on the user's
- * preference stored in local storage.
- */
 function toggleTheme() {
   const isDarkMode = localStorage.getItem("theme") === "dark";
   document.body.classList.toggle("dark", !isDarkMode);
@@ -73,14 +41,6 @@ function toggleTheme() {
 document
   .getElementById("theme-switcher")
   .addEventListener("change", toggleTheme);
-
-/**
- * The function `loadPlaylists` dynamically loads YouTube playlists onto a webpage using data from an
- * array of playlist objects.
- * @returns The `loadPlaylists` function returns nothing (`undefined`) explicitly, but it does have a
- * `return;` statement that will exit the function early if the `playlistContainer` element is not
- * found in the document.
- */
 
 function loadPlaylists() {
   const playlists = [
@@ -153,11 +113,6 @@ function loadPlaylists() {
     playlistContainer.appendChild(colDiv);
   });
 }
-
-/**
- * The function `showMenu` toggles the responsive class on an element with the id "myTopnav" to show or
- * hide a menu.
- */
 function showMenu() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -166,8 +121,6 @@ function showMenu() {
     x.className = "topnav";
   }
 }
-
-// Call loadVideos after the page is fully rendered
 window.onload = function () {
   initializeLanguage();
   initializeTheme();
